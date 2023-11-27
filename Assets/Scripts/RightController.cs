1282e4194915f2c6e8920f0a698c889a5f955735
mmills2 <30macm@gmail.com>
2023-11-27 15:05:21 -0500

    delete button

--- Assets/Scripts/RightController.cs
+++ Assets/Scripts/RightController.cs
@@ -5,20 +5,50 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR;
 using static UnityEditor.Progress;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class RightController : MonoBehaviour
 {
     public InputActionProperty primaryButton;
+    public InputActionProperty secondaryButton;
+
+    public XRRayInteractor rayInteractor;
 
     public GameObject prefab;
+    public XRBaseInteractable heldObject;
 
     private void OnEnable()
     {
         primaryButton.action.started += SpawnAPlatform;
+        secondaryButton.action.started += DeletePlatform;
+
+        rayInteractor = GetComponent<XRRayInteractor>();
+        rayInteractor.selectEntered.AddListener(SelectEntered);
+        rayInteractor.selectExited.AddListener(SelectExited);
+
     }
+
+    private void SelectEntered(SelectEnterEventArgs arg0)
+    {
+        heldObject = (XRBaseInteractable)arg0.interactableObject;
+    }
+    private void SelectExited(SelectExitEventArgs arg0)
+    {
+        heldObject = null;
+    }
+
     private void SpawnAPlatform(InputAction.CallbackContext context)
     {
         Instantiate(prefab, gameObject.transform.position, gameObject.transform.rotation);
-
     }
+
+    private void DeletePlatform(InputAction.CallbackContext context)
+    {
+        if (heldObject.tag == "Platform")
+        {
+            Destroy(heldObject.gameObject);
+        }
+    }
+
+    
 }
