c53bfdee99fa7836bbbb1064aad78020795bf2e7
mmills2 <30macm@gmail.com>
2023-11-29 13:26:28 -0500

    tele glitch

--- Assets/Scripts/LevelGoalScript.cs
+++ Assets/Scripts/LevelGoalScript.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.XR.CoreUtils;
 using UnityEngine;
 
 public class LevelGoalScript : MonoBehaviour
 {
     //public TextMeshPro winText;
     public GameObject ball;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +25,7 @@ public class LevelGoalScript : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         //winText.text = "YOU WIN";
+        player.GetComponent<XROrigin>().MoveCameraToWorldLocation(new Vector3(0, player.GetComponent<XROrigin>().Camera.transform.position.y, 25));
         ball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
     }
 }
